690cdac96de9007c8da969da50639245bac45c3a
Willian Silva <silva.williansantos@gmail.com>
2022-02-02 07:55:08 -0300

    Add connectWallet button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import githubLogo from "./assets/github-logo.svg";
 import "./App.css";
 
@@ -7,6 +7,54 @@ const GITHUB_HANDLE = "willian2s";
 const GITHUB_LINK = `https://github.com/${GITHUB_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const { ethereum } = window;
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      } else {
+        console.log("Yo have the ethereum object: ", ethereum);
+
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log(`Found authorized account: ${account}`);
+          setCurrentAccount(account);
+        } else {
+          console.log("Not found authorized accouunt!");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log(`Connected: ${accounts[0]}`);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -14,7 +62,18 @@ const App = () => {
           <p className="header gradient-text">Attack On Titans - Metaverse</p>
           <p className="sub-text">Team up to protect the Metaverse!</p>
           <div className="connect-wallet-container">
-            <img src="https://i.gifer.com/68sE.gif" alt="Monty Python Gif" />
+            <img
+              src="https://i.gifer.com/68sE.gif"
+              alt="Attack on Titans Gif"
+            />
+            {!currentAccount && (
+              <button
+                className="cta-button connect-wallet-button"
+                onClick={connectWallet}
+              >
+                Connect Wallet To Get Started
+              </button>
+            )}
           </div>
         </div>
         <div className="footer-container">
